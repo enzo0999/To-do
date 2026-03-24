2a0417022601bb8fc7d23d7da6ae9f4a10c61305
enzo0999 <enzogaldino@colegiopaulodetarso.com.br>
2026-03-24 14:20:33 +0000

    dheuid

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const taskBtn = document.querySelector("#taskBtn")
-const taskList = document.querySelector("#taskList")
-const taskInput = document.querySelector("#taskInput")
+const taskBtn = document.querySelector("#taskBtn") 
+    const taskList = document.querySelector("#taskList") 
+    const taskInput = document.querySelector("#taskInput") 
 
-taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI !!!")
-})
+    taskBtn.addEventListener("click", function(){
+        const li = document.createElement('li');
+        const taskInputValue = taskInput.value;
+        li.textContent = taskInputValue;
+        taskList.appendChild(li);   
+    })
